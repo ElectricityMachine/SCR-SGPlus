69a8935218e38570d8c336607bf21da9f8188eff
ElectricityMachine <47489506+ElectricityMachine@users.noreply.github.com>
2023-11-10 22:57:18 -0500

    Update README for 0.4 (#13)
    * Document new installation method and update info
    
    * Increment version and enable update checker
    
    * Format files and remove .vscode dir
    
    * Remove demo gif in favor of future video
    
    * Don't treat AutoIt license as code
    
    * Exclude html files from GH Linguist stats

--- settings.py
+++ settings.py
@@ -1,8 +1,7 @@
-VERSION = "v0.3.1-alpha"
-AVG_FPS = 60  # Set your average FPS here
-AVG_PING = 0  # Set your average ping here
-DEBUG_ENABLED = True  # If true, will enable debug prints if they're specified
-UPDATE_CHECK_ENABLED = False  # If true, will run check_for_update() on startup
+AVG_FPS = 45  # Set your average FPS here (default 45)
+AVG_PING = 0  # Set your average ping here (default 0)
+DEBUG_ENABLED = False  # If true, will enable debug logging
+UPDATE_CHECK_ENABLED = True  # If true, will check for updates on startup
 
 
 class Colors:
