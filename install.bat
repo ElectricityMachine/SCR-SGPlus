ad8033062f6596649de7a36524b3f0fdfe1e9320
ElectricityMachine <47489506+ElectricityMachine@users.noreply.github.com>
2023-12-13 15:47:06 -0500

    Fix install script not working

--- install.bat
+++ install.bat
@@ -1,18 +1,19 @@
 @ECHO OFF
-echo "This script will install SG+'s script dependencies and create a virtual environment for you."
-echo "If you have not installed Python 3.12, please exit the script and install it from https://python.org"
+echo This script will install SG+'s script dependencies and create a virtual environment for you.
+echo If you have not installed Python 3.12, please exit the script and install it from https://python.org
 choice /m "Have you installed Python 3.12?"
 if %errorlevel% equ 1 goto confirmed
 if %errorlevel% equ 2 goto request_install
 
 :confirmed
-echo "Creating venv..."
+echo Creating venv...
 call python -m venv .venv
-echo "Installing dependencies..."
-cd ..\..
+echo Installing dependencies...
 .venv\Scripts\python.exe -m pip install -r requirements.txt
-PAUSE
+echo SG+ install successful. Happy signalling!
+pause
+exit
 
 :request_install
-echo "You have indicated you have not installed Python 3.12. Please do so, then run the script again."
+echo You have indicated you have not installed Python 3.12. Please do so, then run the script again.
 PAUSE
