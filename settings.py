070384e2e2a3d7002e6b79cc621d0242899cab87
ElectricityMachine <47489506+ElectricityMachine@users.noreply.github.com>
2023-12-18 14:51:55 -0500

    Change default FPS to 40
    This may prevent false negative from occurring due to script speed.
    Further improvements are needed to test and address this issue.

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
-AVG_FPS = 45  # Set your average FPS here (default 45)
+AVG_FPS = 40  # Set your average FPS here (default 40)
 AVG_PING = 0  # Set your average ping here (default 0)
 DEBUG_ENABLED = False  # If true, will enable debug logging
 UPDATE_CHECK_ENABLED = True  # If true, will check for updates on startup
