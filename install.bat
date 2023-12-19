afd81b0c299c8c424e5dc9c21fc8f1d69fb45920
ElectricityMachine <47489506+ElectricityMachine@users.noreply.github.com>
2023-12-19 15:19:17 -0500

    Delete .venv if it exists before install
    Closes #33

--- install.bat
+++ install.bat
@@ -1,12 +1,14 @@
 @ECHO OFF
 echo This script will install SG+'s script dependencies and create a virtual environment for you.
 echo If you have not installed Python 3.12, please exit the script and install it from https://python.org
-choice /m "Have you installed Python 3.12?"
+choice /m "Have you installed Python 3.12? "
 if %errorlevel% equ 1 goto confirmed
 if %errorlevel% equ 2 goto request_install
 
 :confirmed
 echo Creating venv...
+if exist ".venv" rmdir /S /Q .venv
+
 call python -m venv .venv
 echo Installing dependencies...
 .venv\Scripts\python.exe -m pip install -r requirements.txt
